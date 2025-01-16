47a6daa02ca9b2e92a0ac90337ed23ccbd032ae3
NAJIB <najib.azmi2019@gmail.com>
2025-01-17 00:55:39 +0100

    first commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -5,24 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping
-	public List<Student> helloWorld() {
-		return List.of(new Student(1,"NAJIB AZMI","najib@gmail.com", LocalDate.of(2004,01,20),21),
-				new Student(2,"HAMZA BERCHIL","hamza@gmail.com", LocalDate.of(2002,05,20),23)
-				);
-	}
-	@GetMapping("/test")
-	public String test() {
-		return "Test!";
-	}
+
+
 }
