c42e1477a13abeb81498d1be74b932f8069eca74
NAJIB <najib.azmi2019@gmail.com>
2025-01-17 00:01:51 +0100

    first commit

--- demo/src/test/java/com/example/demo/student/Student.java
+++ demo/src/test/java/com/example/demo/student/Student.java
@@ -1,4 +1,27 @@
 package com.example.demo.student;
 
+import java.time.LocalDate;
+
 public class Student {
+private Long id;
+private String name;
+private String email;
+private LocalDate dob;
+private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+    public LocalDate getDob() {
+        return dob;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
 }
