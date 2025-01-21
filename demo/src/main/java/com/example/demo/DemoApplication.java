963819b0f75aeefa6a77b6cc4fef77e8f9b73141
NAJIB <najib.azmi2019@gmail.com>
2025-01-22 00:40:04 +0100

    first commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
