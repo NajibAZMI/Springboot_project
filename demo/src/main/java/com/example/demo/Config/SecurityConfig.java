fe6c776f1fcdd94bb932b977f8d2edc4c3f4f877
NAJIB <najib.azmi2019@gmail.com>
2025-01-22 14:50:41 +0100

    first commit

--- demo/src/main/java/com/example/demo/Config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.example.demo.Config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()            // Tout autre URL nécessite une authentification
+                .and()// Active le formulaire de connexion
+                .httpBasic();                  // URL de la page de connexion personnalisée
+
+        return http.build();
+    }
 }
